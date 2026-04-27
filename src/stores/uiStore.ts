0a06b7e7d7500da892380f0dcffc7affc90cba07
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-04-28 08:46:41 +0900

    Add global import center visibility state

--- src/stores/uiStore.ts
+++ src/stores/uiStore.ts
@@ -6,18 +6,26 @@ interface UIState {
   mode: AppMode;
   sidebarVisible: boolean;
   inspectorVisible: boolean;
+  importCenterVisible: boolean;
 
   setMode: (mode: AppMode) => void;
   toggleSidebar: () => void;
   toggleInspector: () => void;
+  openImportCenter: () => void;
+  closeImportCenter: () => void;
+  toggleImportCenter: () => void;
 }
 
 export const useUIStore = create<UIState>((set) => ({
   mode: 'editor',
   sidebarVisible: true,
   inspectorVisible: true,
+  importCenterVisible: false,
 
   setMode: (mode) => set({ mode }),
   toggleSidebar: () => set((s) => ({ sidebarVisible: !s.sidebarVisible })),
   toggleInspector: () => set((s) => ({ inspectorVisible: !s.inspectorVisible })),
+  openImportCenter: () => set({ importCenterVisible: true, mode: 'editor' }),
+  closeImportCenter: () => set({ importCenterVisible: false }),
+  toggleImportCenter: () => set((s) => ({ importCenterVisible: !s.importCenterVisible, mode: 'editor' })),
 }));
